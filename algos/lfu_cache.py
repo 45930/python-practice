a4f4f80c72cbb063c9d9cd04b11b46df08ca5cba
Coby <contact@45930.xyz>
2023-10-17 23:28:30 -0400

    speed up lfu with ordered dict

--- algos/lfu_cache.py
+++ algos/lfu_cache.py
@@ -1,16 +1,17 @@
+from collections import defaultdict, OrderedDict
+
+
 class LFUCache:
     __slots__ = ("capacity", "values", "counters", "min_freq")
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.values = {1: {}}  # freq -> { key -> value}
-        self.counters = {}  # { key -> freq }
+        self.values = defaultdict(OrderedDict)  # freq -> { key -> value}
+        self.counters = OrderedDict()  # { key -> freq }
         self.min_freq = 1
 
     def _get_value(self, key: int) -> int:
-        frequency = self.counters[key]
-        values = self.values[frequency]
-        return values[key]
+        return self.values[self.counters[key]][key]
 
     def _set_value(self, key: int, value: int) -> int:
         self.values[self.counters[key]][key] = value
@@ -20,13 +21,10 @@ class LFUCache:
         value = self.values[freq].pop(key)
         if freq == self.min_freq and len(self.values[freq]) == 0:
             self.min_freq += 1
-        if freq + 1 not in self.values:
-            self.values[freq + 1] = {}
         self.counters[key] = freq + 1
         self.values[freq + 1].update({key: value})
 
     def get(self, key: int) -> int:
-        print(f"Get {key}")
         if key not in self.counters:
             return -1
 
@@ -34,14 +32,12 @@ class LFUCache:
         return self._get_value(key)
 
     def put(self, key: int, value: int) -> None:
-        print(f"Put {key}")
         if key in self.counters:
             self.update_frequency(key)
             self._set_value(key, value)
         else:
             if len(self.counters) == self.capacity:
-                lfu_key = list(self.values[self.min_freq].keys())[0]
-                self.values[self.min_freq].pop(lfu_key)
+                lfu_key, _ = self.values[self.min_freq].popitem(last=False)
                 self.counters.pop(lfu_key)
             self.counters[key] = 1
             self.values[1].update({key: value})
